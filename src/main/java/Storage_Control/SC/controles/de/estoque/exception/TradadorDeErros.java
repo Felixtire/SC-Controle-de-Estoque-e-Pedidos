22fa514f6529034de06308b3749d31e6a7211869
Wellington Felix <warlindo0@gmail.com>
2026-02-19 18:02:46 -0300

    feat: Docker

--- src/main/java/Storage_Control/SC/controles/de/estoque/exception/TradadorDeErros.java
+++ src/main/java/Storage_Control/SC/controles/de/estoque/exception/TradadorDeErros.java
@@ -1,20 +1,20 @@
-package Storage_Control.SC.controles.de.estoque.exception;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class TradadorDeErros {
-
-
-    @ExceptionHandler(ProdutoException.class)
-    public ResponseEntity<String> tratarProdutoException(ProdutoException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
-
-    @ExceptionHandler(PedidoException.class)
-    public ResponseEntity<String> tratarPedidoException(PedidoException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
-}
+//package Storage_Control.SC.controles.de.estoque.exception;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//@RestControllerAdvice
+//public class TradadorDeErros {
+//
+//
+//    @ExceptionHandler(ProdutoException.class)
+//    public ResponseEntity<String> tratarProdutoException(ProdutoException ex) {
+//        return ResponseEntity.badRequest().body(ex.getMessage());
+//    }
+//
+//    @ExceptionHandler(PedidoException.class)
+//    public ResponseEntity<String> tratarPedidoException(PedidoException ex) {
+//        return ResponseEntity.badRequest().body(ex.getMessage());
+//    }
+//}
--- src/test/java/Storage_Control/SC/controles/de/estoque/ScControlesDeEstoqueApplicationTests.java
+++ src/test/java/Storage_Control/SC/controles/de/estoque/ScControlesDeEstoqueApplicationTests.java
@@ -1,13 +1,13 @@
-package Storage_Control.SC.controles.de.estoque;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ScControlesDeEstoqueApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package Storage_Control.SC.controles.de.estoque;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ScControlesDeEstoqueApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
