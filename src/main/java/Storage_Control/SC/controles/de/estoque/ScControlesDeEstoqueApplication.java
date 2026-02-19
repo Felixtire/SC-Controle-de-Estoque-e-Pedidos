da42f465b6eb232d8428a41d9b01588a5ad54ff8
Wellington Felix <warlindo0@gmail.com>
2026-02-19 17:24:49 -0300

    feat: log de sucesso

--- src/main/java/Storage_Control/SC/controles/de/estoque/ScControlesDeEstoqueApplication.java
+++ src/main/java/Storage_Control/SC/controles/de/estoque/ScControlesDeEstoqueApplication.java
@@ -1,13 +1,17 @@
 package Storage_Control.SC.controles.de.estoque;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ScControlesDeEstoqueApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ScControlesDeEstoqueApplication.class, args);
+        log.info("##########Aplicação de Controle de Estoque iniciada com sucesso!##########");
 	}
 
 }
