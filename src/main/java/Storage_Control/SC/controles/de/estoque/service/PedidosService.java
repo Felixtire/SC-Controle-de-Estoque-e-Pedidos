fe3e9ef0ab5f4f5e9fa87c0f6946e0c6e85147f1
Wellington Felix <warlindo0@gmail.com>
2026-02-24 15:45:40 -0300

    feat: Cors

--- src/main/java/Storage_Control/SC/controles/de/estoque/service/PedidosService.java
+++ src/main/java/Storage_Control/SC/controles/de/estoque/service/PedidosService.java
@@ -21,8 +21,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 @Service
 public class PedidosService {
